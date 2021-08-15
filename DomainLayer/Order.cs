866128a70cb6372b1960142cd02269efd0b56a4d
Jay Krishna Reddy <jaykrishnareddy@gmail.com>
2021-08-15 21:03:39 +0530

    Added Changes !

---
+++ DomainLayer/Order.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DomainLayer
+{
+   public class Order
+    {
+       public int OrderId { get; set;}
+       public string OrderDetails { get; set; }
+       public bool IsActive { get; set; }
+       public DateTime OrderedDate { get; set; }
+    }
+}
---
+++ DomainLayer/Product.cs
@@ -0,0 +1,10 @@
+﻿namespace DomainLayer
+{
+    public class Product
+    {
+       public int ProductId { get; set; }
+       public string Name { get; set; }
+       public float Price { get; set; }
+       public bool isDisCountApplied  { get; set; }
+    }
+}
