57a6e9ed0efa4cd91ec165eace1490950e1d6a98
Jay Krishna Reddy <jaykrishnareddy@gmail.com>
2021-08-17 09:44:23 +0530

    Added XML Comments.!

--- OnionArchitecture_CQRS_Dapper/Controllers/OrderController.cs
+++ OnionArchitecture_CQRS_Dapper/Controllers/OrderController.cs
@@ -7,8 +7,17 @@ namespace OnionArchitecture_CQRS_Dapper.Controllers
 {
     public class OrderController : BaseController
     {
+        /// <summary>
+        /// Save newly added order to database
+        /// </summary>
+        /// <param name="command"></param>
+        /// <returns></returns>
         [HttpPost(nameof(SaveOrderData))]
         public async Task<IActionResult> SaveOrderData(CreateOrUpdateOrderCommand command) => Ok(await Mediator.Send(command));
+        /// <summary>
+        /// Fetch all data from the Orders table.
+        /// </summary>
+        /// <returns></returns>
         [HttpGet]
         public async Task<IActionResult> GetAllOrders() => Ok(await Mediator.Send(new GetAllOrdersQuery()));
     }
--- OnionArchitecture_CQRS_Dapper/Controllers/ProductController.cs
+++ OnionArchitecture_CQRS_Dapper/Controllers/ProductController.cs
@@ -7,8 +7,17 @@ namespace OnionArchitecture_CQRS_Dapper.Controllers
 {
     public class ProductController : BaseController
     {
+        /// <summary>
+        /// Delete Product from the Products Table
+        /// </summary>
+        /// <param name="command"></param>
+        /// <returns></returns>
         [HttpDelete(nameof(DeleteProduct))]
         public async Task<IActionResult> DeleteProduct(DeleteProductByIdCommand command) => Ok(await Mediator.Send(command));
+        /// <summary>
+        /// Fetch all Product Data from the Database
+        /// </summary>
+        /// <returns></returns>
         [HttpGet]
         public async Task<IActionResult> GetAllProducts() => Ok(await Mediator.Send(new GetAllProductsQuery()));
     }
